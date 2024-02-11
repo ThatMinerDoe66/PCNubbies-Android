83e8c4c930a6a795ea05c88dbde639533fbe6396
Joaquin Gabriel Camangeg <joaquingabrielcamangeg6@gmail.com>
2024-02-11 23:30:26 +0800

    First Commit

---
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joaquingabriel.camangeg.block1.p1.pcnubbies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
