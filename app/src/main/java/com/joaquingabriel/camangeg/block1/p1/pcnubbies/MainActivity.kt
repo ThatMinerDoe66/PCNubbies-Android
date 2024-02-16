ac245692db8c81a6c709442efb982f162a195185
Joaquin Gabriel Camangeg <joaquingabrielcamangeg6@gmail.com>
2024-02-16 23:59:18 +0800

    Quick fix on Navbar

--- app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/MainActivity.kt
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/MainActivity.kt
@@ -2,10 +2,41 @@ package com.joaquingabriel.camangeg.block1.p1.pcnubbies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.joaquingabriel.camangeg.block1.p1.pcnubbies.fragments.HomeFragment
+import com.joaquingabriel.camangeg.block1.p1.pcnubbies.fragments.ShopFragment
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<CurvedBottomNavigation>(R.id.bottomNavigation)
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(1, "Home", R.drawable.baseline_home_button)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(2, "Shop", R.drawable.baseline_home_button)
+        )
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                1 ->{
+                    replaceFragment(ShopFragment())
+                }
+                2 ->{
+                    replaceFragment((HomeFragment()))
+                }
+            }
+        }
+
     }
+    private fun replaceFragment(fragment:Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
 }
--- app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/ShopFragment.kt
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/ShopFragment.kt
@@ -14,6 +14,6 @@ class ShopFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_shop, container, false)
     }
 }
