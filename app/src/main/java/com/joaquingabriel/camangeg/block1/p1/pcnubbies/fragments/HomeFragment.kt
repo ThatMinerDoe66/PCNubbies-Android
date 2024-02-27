07521ec704b0f74a2deaca6093afc9e094f95c41
Joaquin Gabriel Camangeg <joaquingabrielcamangeg6@gmail.com>
2024-02-27 19:29:41 +0800

    >Added new fragments to be accessible. >Updated backend navigation

--- app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/HomeFragment.kt
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/HomeFragment.kt
@@ -7,11 +7,34 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.joaquingabriel.camangeg.block1.p1.pcnubbies.R
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigation = view.findViewById<CurvedBottomNavigation>(R.id.bottomNavigation)
+
+        //call images from xml
+        val shop : ImageView = view.findViewById(R.id.shop)
+        val guides : ImageView = view.findViewById(R.id.guides)
+
+
+        //onClickListener
+        guides.setOnClickListener{
+            //bottomNavigation.show(2)  <<Wtf, I can't update the navbar on this one!
+            replaceFragment(GuideFragment())
+        }
+
+        shop.setOnClickListener{
+            //bottomNavigation.show(0)   <<AAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHH
+            replaceFragment(ShopFragment())
+        }
+
+
+
+
 
 
     }
@@ -22,6 +45,20 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun replaceFragment(fragment:Fragment){
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
+    private fun addFragment(fragment:Fragment){
+        requireActivity().supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainer, fragment)
+            .commit()
     }
 
 }
