1a5f9b372541760fc18df55b60fd14b281a923a8
genbalut <quenanomarion3@gmail.com>
2024-03-05 15:44:29 +0800

    Refracted Name and removed Search bar

--- app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/ProfileFragment.kt
+++ app/src/main/java/com/joaquingabriel/camangeg/block1/p1/pcnubbies/fragments/ProfileFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.joaquingabriel.camangeg.block1.p1.pcnubbies.R
 
-class CartFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
 
 
     }
